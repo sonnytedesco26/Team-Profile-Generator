cf66ad35ffa1b6628021f67426bf5b5f6cc30458
Sonny Tedesco <sonny.tedesco@darkmatter2bd.com>
2022-06-04 15:34:01 -0400

    changed it so that questions would be included in the lib files for each role

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
 const inquirer = require('inquirer');
+
+const {Engineer, engineerQuestions} = require('./lib/engineer');
+const {Intern, internQuestions} = require('./lib/intern');
+const {Manager, managerQuestions} = require('./lib/manager');
+
+teamArray=[];
+
+const employeePrompt=()=>{
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'employeeRole',
+            message: 'What type of employee do you want to add?',
+            choices:[
+                {name:'Engineer', value:'engineerAdd'},
+                {name:'Intern', value:'internAdd'},
+                {name:'Manager', value:'managerAdd'},
+                {name:'None', value:'none'},
+            ]
+        }
+    ]).then(response =>{
+        if(response.employeeRole == 'engineerAdd'){
+            engineerPrompt();
+        };
+        if(response.employeeRole == 'internAdd'){
+            internPrompt();
+        };
+        if(response.employeeRole == 'managerAdd'){
+            managerPrompt();
+        };
+        if(answer.employeeRole == 'none'){
+
+        }
+    })
+};
+
+
+const engineerPrompt=()=>{
+    inquirer.prompt(engineerQuestions)
+    .then((response) => {
+        response = new Engineer(response.id,response.name,response.email,response.github)
+        teamArray.push(response);
+
+        return employeePrompt();
+    })
+};
+
+const internPrompt=()=>{
+    inquirer.prompt(internQuestions)
+    .then((response) => {
+        response = new Intern(response.id,response.name,response.email,response.school)
+        teamArray.push(response);
+
+        return employeePrompt();
+    })
+};
+
+const managerPrompt=()=>{
+    inquirer.prompt(managerQuestions)
+    .then((response) => {
+        response = new Manager(response.id,response.name,response.email,response.officeNumber)
+        teamArray.push(response);
+
+        return employeePrompt();
+    })
+};
